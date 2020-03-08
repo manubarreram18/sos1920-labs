2be1da4cd610cdcabdbb00d057a1c6ca27a4f978
manubarreram18 <manuelbarreramoya@yahoo.es>
2020-03-08 17:56:40 +0000

    Update

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const express = require("express");
 
 var app = express();
 
+var port = process.env.PORT || 80;
+
+app.use("/", express.static("./public"))
+
 app.get("/cool",(request,response) => {
 	response.send("<html>"+cool()+"</html>");
 });
 
-app.listen(80);
+app.listen(port, () => {
+		console.log("server ready");
+});
 
-console.log("server ready");
+console.log("Starting server...");
