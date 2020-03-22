f2d0d187236fb2d50f0905889aac9d953fa1e15e
manubarreram18 <manuelbarreramoya@yahoo.es>
2020-03-22 18:36:25 +0000

    update

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const cool = require("cool-ascii-faces");
 const express = require("express");
-
+const bodyParser = require("body-parser");
 var app = express();
 
 var port = process.env.PORT || 80;
+
+app.use(bodyParser.json());
 
 app.use("/", express.static("./public"))
 
 app.get("/cool",(request,response) => {
 	response.send("<html>"+cool()+"</html>");
 });
+
+var contacts = [
+	{
+		name: "peter",
+		phone: 123456
+	},
+	{
+		name: "pablo",
+		phone: 78945
+	}
+];
+
+const BASE_API_URL = "/api/v1";
+
+app.get(BASE_API_URL+"/contacts", (req, res) => {
+	res.send(JSON.stringify(contacts, null, 2));
+});
+app.post(BASE_API_URL+"/contacts", (req, res) => {
+	contacts.push(req.body);
+	res.sendStatus(201,"CREATED");
+});
+
 
 app.listen(port, () => {
 		console.log("server ready");
